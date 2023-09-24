0baf44b387ff05a3140ecf994ee34e202d999bf7
Boonyawat Ariyawatkul <boonyawat.ariyawatkul@kcl.ac.uk>
2023-09-24 21:48:44 +0100

    Add ValueError in primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes > 0:
+        list = []
+        num = 2
+        list.append(num)
+        num = num + 1
+        while len(list) < number_of_primes:
+            for divisor in range(2,num):
+                if num % divisor == 0:
+                    break
+            else:
+                list.append(num)
+            num += 1
+
+        return list
+    else:
+        raise ValueError(f"{number_of_primes} should be positive")
